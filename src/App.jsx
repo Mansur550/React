fb112b1239bc4308774dd6c27ad55691577d7695
Mansur550 <mansurislam59@gmail.com>
2025-12-08 00:56:20 +0600

    Fist Commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+const Card = () =>{
+  return (
+
+    <div>
+      <h2>Card Component</h2>
+    </div>
+  )
+}
+
+const App = () => {
+  return(
+    <div>
+    <h2>Functional Arrow Component</h2>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    </div>
+  )
+
+}
+
+export default App
+
